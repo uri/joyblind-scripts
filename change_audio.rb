cead70675ccd8e56d4552d66ba4cf3e7c247d5a2
= <uri.gore@gmail.com>
2011-11-14 16:58:00 -0500

    Added random.

--- change_audio.rb
+++ change_audio.rb
@@ -1,9 +1,15 @@
 # encoding: UTF-8
 
+
+# Open old_index and find which files are podcast files.
+old_index = open('old_index.html').read
+
 str = open('out.txt').read
 
 sub = "\n\n<audio src=\"/content/podcasts/\\1.mp3\" controls=\"controls\"></audio>\n\n"
-reg = /\s*<\s*div\s*>\s*<\s*a\s*href="http:\/\/www.joyblind.com\/content\/podcasts\/(.*).mp3.*/
+new_reg = /\s*<\s*div\s*>\s*<\s*a\s*href="http:\/\/www.joyblind.com\/content\/podcasts\/(.*).mp3.*/
+old_reg = //
+search_reg = //
 
 str.gsub!(reg, sub)
 
